d1b3006b2a7f733d4c04647f6e49609ee5d9463c
ATUMBAC0 <167648065+ATUMBAC0@users.noreply.github.com>
2024-04-27 19:02:48 -0500

    Clase 3

---
+++ Clase3/0_Archivo1.py
@@ -0,0 +1,21 @@
+# esta es una varible
+variable=8
+variable_dos= "texto dos"
+print(variable_dos)
+my_string="thisStringisAwesome"
+print(my_string)
+
+print(my_string.upper()) #coloca en mayusculas
+print(my_string)
+
+# permite mostrar en la terminal de informacion
+"""print("hola mundo")
+print(type (8))
+print(type(1.8))
+print(type("hola mundo"))"""
+
+#para comentar 
+#crt k +ctrio c
+# """ comentar """
+#para descometar 
+#crt k +ctrio u
